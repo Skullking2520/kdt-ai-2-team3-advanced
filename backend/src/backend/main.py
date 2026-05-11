1fb0b00734d13c95545c6923906b92b45d73617d
openLeeWorld <leekipil98@gmail.com>
2026-05-11 12:28:33 +0900

    fix: 루트 의존성에 각 monorepo member 포함해서 uv sync 맞춤

---
+++ backend/src/backend/main.py
@@ -0,0 +1,14 @@
+# src/backend/main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from uv monorepo!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
