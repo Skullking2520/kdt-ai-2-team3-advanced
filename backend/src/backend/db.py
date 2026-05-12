fad99f4fcfbb9a8fced369e3aad8a8b59d4b59e0
openLeeWorld <leekipil98@gmail.com>
2026-05-12 14:41:04 +0900

    feat: dockerfile added for backend

--- backend/src/backend/db.py
+++ backend/src/backend/db.py
@@ -1,14 +1,18 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import find_dotenv, load_dotenv
 from sqlalchemy import Column, Float, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()  # .env파일 로드
+load_dotenv(find_dotenv())  # .env파일 로드, 부모 디렉토리까지 다 뒤지게 함
+
 # 1. DB 연결 설정 (PyMySQL 드라이버 사용)
 # 형식: mysql+pymysql://유저명:비밀번호@호스트:포트/DB명
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "없으면 오류 남")
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://myuser:mypassword@host.docker.internal:3306/mydatabase",
+)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
