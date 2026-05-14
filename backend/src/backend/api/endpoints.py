6f9272acec0792fd232a121bdc2c2b70a6296a26
openLeeWorld <leekipil98@gmail.com>
2026-05-14 09:41:54 +0900

    feat: db erd에 맞게 table 초안 추가

--- backend/src/backend/api/endpoints.py
+++ backend/src/backend/api/endpoints.py
@@ -1,24 +1,19 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..db.session import get_db
 from ..models.inference_log import InferenceLog
 
-router = APIRouter()
+router = APIRouter(prefix="/endpoints")  # tags=[]는 swagger 기능으로 묶임
 
 
-@router.get("/tests")
-def read_tests():
-    return [{"users": "user1"}]
-
-
-# 4. 테스트 API 엔드포인트
+# 테스트 API 엔드포인트
 @router.get("/db_test")
-def db_test(db: Session = Depends(get_db)):
+async def db_test(db: AsyncSession = Depends(get_db)):
     # 가짜 추론 데이터 생성 및 저장
     new_log = InferenceLog(model_name="my_model_v1", prediction=0.98)
-    db.add(new_log)
-    db.commit()
-    db.refresh(new_log)
+    db.add(new_log)  # 메모리에만 추가하므로 await 없음
+    await db.commit()
+    await db.refresh(new_log)
 
     return {"status": "success", "saved_id": new_log.id, "data": new_log}
--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -1,16 +1,25 @@
 # src/backend/main.py
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from .api import endpoints
 from .db.create_tables import create_db_tables
 
-app = FastAPI()
 
-app.include_router(endpoints.router, prefix="/endpoint")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 서버가 시작될 때 테이블 생성
+    await create_db_tables()
+    yield  # 제어권 넘기는 제너레이터
+    # 서버가 종료될 때 실행할 코드가 있다면 여기에 작성
 
-create_db_tables()
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(endpoints.router)
 
 
 @app.get("/")
-def read_root():
+def read_root():  # await할게 없으면 async하지 않는다.
     return {"message": "Hello!"}
