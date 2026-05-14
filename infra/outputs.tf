b78243e9d449152d23ed31bd0ff7148252594653
openLeeWorld <leekipil98@gmail.com>
2026-05-15 01:05:09 +0900

    fix: terraform nic omission fixed

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,3 +6,13 @@ output "server_public_ip" {
 output "db_service_name" {
   value = ncloud_mysql.smishing_db.service_name
 }
+
+output "ssh_private_key" {
+  value       = ncloud_login_key.loginkey.private_key
+  description = "ncp server private key 출력"
+  sensitive   = true
+}
+output "server_initial_password" {
+  value     = data.ncloud_root_password.root_pwd.root_password
+  sensitive = true
+}
