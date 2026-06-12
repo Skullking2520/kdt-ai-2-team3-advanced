669ebb5ffe1332152f300da7467794b4604e8597
openLeeWorld <leekipil98@gmail.com>
2026-06-12 09:54:41 +0900

    fix: fe web_mvp folder deleted

--- frontend/web_mvp/src/utils/API_URL.js
+++
@@ -1,6 +0,0 @@
-const HOME_URL =
-  import.meta.env.VITE_SMISHING_HOME_URL ?? "http://localhost:8000";
-const PREDICT_API_URL = HOME_URL + "/predict";
-const REPORT_API_URL = HOME_URL + "/report";
-
-export { HOME_URL, PREDICT_API_URL, REPORT_API_URL };
